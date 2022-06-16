33d780fc27a074c46d630849b77e33a08bb7933b
SongMinJun <dkaskgkdua@gmail.com>
2022-06-17 01:11:48 +0900

    HTTP html 응답 데이터 세팅

---
+++ src/main/java/hello/spring_mvc/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,36 @@
+package hello.spring_mvc.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name ="responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
+        // 캐시 무효화
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        // 쿠키 세팅
+//        resp.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); // 600초
+        resp.addCookie(cookie);
+
+        // redirect
+//        resp.setStatus(HttpServletResponse.SC_FOUND);
+//        resp.setHeader("Location", "/basic/hello-form.html");
+
+
+        resp.getWriter().write("ok");
+    }
+}
